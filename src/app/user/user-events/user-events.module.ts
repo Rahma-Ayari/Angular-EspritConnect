b0018d7156e6d783a155b6a6ad400ba765dde17a
dorsafmadani <madanidorsaf@gmail.com>
2026-06-04 17:20:21 +0100

    events

--- src/app/user/user-events/user-events.module.ts
+++ src/app/user/user-events/user-events.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { UserEventsRoutingModule } from './user-events-routing.module';
